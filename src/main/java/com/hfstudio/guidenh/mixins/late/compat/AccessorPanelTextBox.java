9a653d2baef1d75279eea73be69046db3b0041d1
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-07-03 11:13:27 +0800

    add support for in page link syntax in quest pages (#35)

---
+++ src/main/java/com/hfstudio/guidenh/mixins/late/compat/AccessorPanelTextBox.java
@@ -0,0 +1,15 @@
+package com.hfstudio.guidenh.mixins.late.compat;
+
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import betterquesting.api2.client.gui.panels.content.PanelTextBox;
+
+@Mixin(value = PanelTextBox.class, remap = false)
+public interface AccessorPanelTextBox {
+
+    @Accessor("hotZones")
+    List<Object> guidenh$getHotZones();
+}
